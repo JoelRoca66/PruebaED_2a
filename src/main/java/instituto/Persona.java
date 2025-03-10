82fe16cdb1379d3b8c5b413496ff3567a4193e63
jroca <joelrocamolano@gmail.com>
2025-03-10 18:40:13 +0100

    Despues de eliminar la rama master

--- src/main/java/instituto/Persona.java
+++ src/main/java/instituto/Persona.java
@@ -4,6 +4,12 @@ import java.time.LocalDate;
 import java.time.Period;
 import java.util.Objects;
 
+/**
+ *Esta clase nos permite crear 
+ * 
+ * @author Joel Roca
+ * @version 1.0.0
+ */
 public class Persona implements Comparable<Persona> {
 
     private Nif nif;
@@ -11,6 +17,9 @@ public class Persona implements Comparable<Persona> {
     private char genero;
     private LocalDate nacimiento;
 
+    /**
+     *
+     */
     public Persona() {
         nif = new Nif();
         nombre = "";
@@ -18,11 +27,24 @@ public class Persona implements Comparable<Persona> {
         nacimiento = LocalDate.of(1990, 1, 1);
     }
 
+    /**
+     *
+     * @param nif
+     */
     public Persona(int nif) {
         this();
         this.nif = new Nif(nif);
     }
 
+    /**
+     *
+     * @param nif
+     * @param nombre
+     * @param genero
+     * @param dia
+     * @param mes
+     * @param ano
+     */
     public Persona(int nif, String nombre, char genero,
             int dia, int mes, int ano) {
         this.nif = new Nif(nif);
@@ -32,42 +54,82 @@ public class Persona implements Comparable<Persona> {
                 = LocalDate.of(ano, mes, dia);
     }
 
+    /**
+     *
+     * @return
+     */
     public Nif getNif() {
         return nif;
     }
 
+    /**
+     *
+     * @param nif
+     */
     public void setNif(Nif nif) {
         this.nif = nif;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getNombre() {
         return nombre;
     }
 
+    /**
+     *
+     * @param nombre
+     */
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
+    /**
+     *
+     * @return
+     */
     public char getGenero() {
         return genero;
     }
 
+    /**
+     *
+     * @param genero
+     */
     public void setGenero(char genero) {
         this.genero = genero;
     }
 
+    /**
+     *
+     * @return
+     */
     public LocalDate getNacimiento() {
         return nacimiento;
     }
 
+    /**
+     *
+     * @param nacimiento
+     */
     public void setNacimiento(LocalDate nacimiento) {
         this.nacimiento = nacimiento;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getEdad() {
         return Period.between(nacimiento, LocalDate.now()).getYears();
     }
 
+    /**
+     *
+     * @return
+     */
     @Override
     public String toString() {
         if (nombre.split(" ").length > 1) {
@@ -78,6 +140,11 @@ public class Persona implements Comparable<Persona> {
         }
     }
 
+    /**
+     *
+     * @param a
+     * @return
+     */
     public boolean equals(Persona a) {
         if (a == null) {
             return false;
@@ -85,6 +152,11 @@ public class Persona implements Comparable<Persona> {
         return a.nif.toString().equals(this.nif.toString());
     }
 
+    /**
+     *
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -101,6 +173,12 @@ public class Persona implements Comparable<Persona> {
         return Objects.equals(this.nif, other.nif);
     }
 
+    /**
+     * Metodo que nos permite comparar personas, para a la hora de crear una
+     * nueva sepamos si esa persona ya existe.
+     * @param o
+     * @return
+     */
     @Override
     public int compareTo(Persona o) {
         return this.nif.toString().compareTo(o.nif.toString());
