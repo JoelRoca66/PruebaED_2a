2b4de88fb379aa5e1cd6640d01f4d5be976abf73
jroca <joelrocamolano@gmail.com>
2025-03-10 17:52:13 +0100

    codigo inicial

---
+++ src/main/java/instituto/Curso.java
@@ -0,0 +1,33 @@
+package instituto;
+
+import java.util.TreeSet;
+
+public class Curso {
+
+    private String nombre;
+    private TreeSet<Persona> listaAlumnos;
+
+    protected String getNombre() {
+        return nombre;
+    }
+
+    public Curso(String nombre) {
+        this.nombre = nombre;
+        listaAlumnos = new TreeSet<>();
+    }
+
+    @Override
+    public String toString() {
+        String s = "--------------------" + nombre + "-----------------\n";
+        s += "NumExp\tNIF\t\tNombre\t\tApellidos\n";
+        s += "-------------------------------------------------\n";
+        for (Persona listaAlumno : listaAlumnos) {
+            s += listaAlumno + "\n";
+        }
+        return s;
+    }
+
+    public void aniadirAlumno(Persona p) {
+        listaAlumnos.add(p);
+    }
+}
