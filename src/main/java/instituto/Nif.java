f9be5485a64dc049146d69f593e076fa542d52a9
jroca <joelrocamolano@gmail.com>
2025-03-10 19:41:01 +0100

    documentada clase Nif

--- src/main/java/instituto/Nif.java
+++ src/main/java/instituto/Nif.java
@@ -1,5 +1,9 @@
 package instituto;
 
+/**
+ * Clase que nos permite crear objetos de tipo Nif
+ * @author Joel Roca
+ */
 public class Nif {
 
     private int numero;
@@ -15,26 +19,48 @@ public class Nif {
         return LETRAS[numero % 23];
     }
 
+    /**
+     * Constructor que permite crear un objeto de tipo Nif con valores por defecto.
+     */
     protected Nif() {
         this.numero = 0;
         this.letra = ' ';
     }
 
+    /**
+     * Constructor que permite crear un objeto de tipo Nif mediante un numero que se le
+     * pase como parámetro.
+     * @param numero
+     */
     protected Nif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     * Método que devuelve un Nif en forma de String
+     * @return
+     */
     @Override
     public String toString() {
         return numero + "-" + letra;
     }
 
+    /**
+     * Metodo que permite cambiar el numero y la letra de un dni, asignándole
+     * un nuevo valor que se le pasa como parametro (numero, letra automática).
+     * @param numero
+     */
     protected void setNif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     * Metodo que permite comprobar si 2 objetos de tipo Nif son iguales o no.
+     * @param obj
+     * @return
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
